98eb00ef1a8f9669b756f8a25a573209c06c14d6
Ken Lauer <klauer@slac.stanford.edu>
2021-03-19 10:38:33 -0700

    STY: missed source files for black

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- General configuration ------------------------------------------------
 
@@ -29,30 +30,32 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-	'sphinx.ext.napoleon',
-	'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
-#source_suffix = '.rst'
+source_suffix = [".rst", ".md"]
+# source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'dmtoqt'
-copyright = u'2017, Bob Gunion/LBNL'
-author = u'Bob Gunion/LBNL'
+project = u"dmtoqt"
+copyright = u"2017, Bob Gunion/LBNL"
+author = u"Bob Gunion/LBNL"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -60,6 +63,7 @@ author = u'Bob Gunion/LBNL'
 #
 # The short X.Y version.
 import version as v
+
 version = v.version
 # The full version, including alpha/beta/rc tags.
 release = v.version
@@ -74,10 +78,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
@@ -88,7 +92,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -99,7 +103,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -107,12 +111,12 @@ html_static_path = ['_static']
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-        'donate.html',
+    "**": [
+        "about.html",
+        "navigation.html",
+        "relations.html",  # needs 'show_related': True theme option to display
+        "searchbox.html",
+        "donate.html",
     ]
 }
 
@@ -120,7 +124,7 @@ html_sidebars = {
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'dmtoqtdoc'
+htmlhelp_basename = "dmtoqtdoc"
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -129,15 +133,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -147,8 +148,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'dmtoqt.tex', u'dmtoqt Documentation',
-     u'Bob Gunion/LBNL', 'manual'),
+    (master_doc, "dmtoqt.tex", u"dmtoqt Documentation", u"Bob Gunion/LBNL", "manual"),
 ]
 
 
@@ -156,10 +156,7 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'dmtoqt', u'dmtoqt Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "dmtoqt", u"dmtoqt Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output -------------------------------------------
@@ -168,25 +165,29 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'dmtoqt', u'dmtoqt Documentation',
-     author, 'dmtoqt', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "dmtoqt",
+        u"dmtoqt Documentation",
+        author,
+        "dmtoqt",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
 
 
-
-
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {"https://docs.python.org/": None}
 
 
 # skip_member function, issued when autodoc decides whether to include a member
 # ("member" may be one of "module", "class", "exception", "function", "method", "attribute")
 def dmtoqt_skip_member(app, what, name, obj, skip, options):
-	if what == 'module' and name in ('setup', 'version', 'src.widgets.widget_template'):
-		return True
-	return None
+    if what == "module" and name in ("setup", "version", "src.widgets.widget_template"):
+        return True
+    return None
+
 
 def setup(app):
-	app.connect('autodoc-skip-member', dmtoqt_skip_member)
-
+    app.connect("autodoc-skip-member", dmtoqt_skip_member)
